1563a2f01f86b83e875f5108a0446b77e2e7fca2
scarletsky <scarletsky1025@gmail.com>
2018-01-11 22:28:06 +0800

    chore: convert Vec abstract class to interface

--- src/math/vec.ts
+++ src/math/vec.ts
@@ -1,23 +1,21 @@
-export default abstract class Vec {
-    data: Float32Array;
-
-    abstract add(b: Vec): Vec;
-    abstract add2(a: Vec, b: Vec): Vec;
-    abstract clone(): Vec;
-    abstract copy(b: Vec): Vec;
-    abstract cross(a: Vec, b: Vec): Vec;
-    abstract dot(b: Vec): number;
-    abstract equals(b: Vec): boolean;
-    abstract length(): number;
-    abstract lengthSq(): number;
-    abstract lerp(a: Vec, b: Vec, alpha: number): Vec;
-    abstract mul(b: Vec): Vec;
-    abstract mul2(a: Vec, b: Vec): Vec;
-    abstract normalize(): Vec;
-    abstract project(b: Vec): Vec;
-    abstract scale(scalar: number): Vec;
-    abstract set(x: number, y: number, z: number): Vec;
-    abstract sub(b: Vec): Vec;
-    abstract sub2(a: Vec, b: Vec): Vec;
-    abstract toString(): string;
+export default interface Vec {
+    add(b: Vec): Vec;
+    add2(a: Vec, b: Vec): Vec;
+    clone(): Vec;
+    copy(b: Vec): Vec;
+    cross?(a: Vec, b: Vec): Vec;
+    dot(b: Vec): number;
+    equals(b: Vec): boolean;
+    length(): number;
+    lengthSq(): number;
+    lerp(a: Vec, b: Vec, alpha: number): Vec;
+    mul(b: Vec): Vec;
+    mul2(a: Vec, b: Vec): Vec;
+    normalize(): Vec;
+    project?(b: Vec): Vec;
+    scale(scalar: number): Vec;
+    set(x: number, y: number, z: number): Vec;
+    sub(b: Vec): Vec;
+    sub2(a: Vec, b: Vec): Vec;
+    toString(): string;
 }
