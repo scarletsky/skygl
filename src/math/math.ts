1d4ec8f351b210587d7b1b41448458a6594897ea
scarletsky <scarletsky1025@gmail.com>
2018-01-13 15:16:48 +0800

    chore: export all stuff in math

--- src/math/math.ts
+++ src/math/math.ts
@@ -1,16 +1,16 @@
-const DEG_TO_RAD = Math.PI / 180;
-const RAD_TO_DEG = 180 / Math.PI;
-const INV_LOG = 1 / Math.log(2);
-const intToBytes = intToBytes32;
-const bytesToInt = bytesToInt32;
+export const DEG_TO_RAD = Math.PI / 180;
+export const RAD_TO_DEG = 180 / Math.PI;
+export const INV_LOG = 1 / Math.log(2);
+export const intToBytes = intToBytes32;
+export const bytesToInt = bytesToInt32;
 
-function clamp(value: number, min: number, max: number): number {
+export function clamp(value: number, min: number, max: number): number {
     if (value >= max) return max;
     if (value <= min) return min;
     return value;
 }
 
-function intToBytes24(i: number): number[] {
+export function intToBytes24(i: number): number[] {
     let r, g, b;
 
     r = (i >> 16) & 0xff;
@@ -20,7 +20,7 @@ function intToBytes24(i: number): number[] {
     return [r, g, b];
 }
 
-function intToBytes32(i: number): number[] {
+export function intToBytes32(i: number): number[] {
     let r, g, b, a;
 
     r = (i >> 24) & 0xff;
@@ -31,7 +31,7 @@ function intToBytes32(i: number): number[] {
     return [r, g, b, a];
 }
 
-function bytesToInt24(r: number | number[], g?: number, b?: number): number {
+export function bytesToInt24(r: number | number[], g?: number, b?: number): number {
     if (Array.isArray(r)) {
         b = r[2];
         g = r[1];
@@ -41,7 +41,7 @@ function bytesToInt24(r: number | number[], g?: number, b?: number): number {
 
 }
 
-function bytesToInt32(r: number | number[], g?: number, b?: number, a?: number): number {
+export function bytesToInt32(r: number | number[], g?: number, b?: number, a?: number): number {
     if (Array.isArray(r)) {
         a = r[3];
         b = r[2];
@@ -55,11 +55,11 @@ function bytesToInt32(r: number | number[], g?: number, b?: number, a?: number):
     return ((r << 24) | (g << 16) | (b << 8) | a) >>> 32;
 }
 
-function lerp(a: number, b: number, alpha: number): number {
+export function lerp(a: number, b: number, alpha: number): number {
     return a + (b - a) * clamp(alpha, 0, 1);
 }
 
-function lerpAngle(a: number, b: number, alpha: number): number {
+export function lerpAngle(a: number, b: number, alpha: number): number {
     if (b - a > 180) {
         b -= 360;
     }
@@ -69,11 +69,11 @@ function lerpAngle(a: number, b: number, alpha: number): number {
     return lerp(a, b, clamp(alpha, 0, 1));
 }
 
-function powerOfTwo(x: number): boolean {
+export function powerOfTwo(x: number): boolean {
     return ((x !== 0) && !(x & (x - 1)));
 }
 
-function nextPowerOfTwo(value: number): number {
+export function nextPowerOfTwo(value: number): number {
     value--;
     value = (value >> 1) | value;
     value = (value >> 2) | value;
@@ -84,11 +84,11 @@ function nextPowerOfTwo(value: number): number {
     return value;
 }
 
-function random(min: number, max: number): number {
+export function random(min: number, max: number): number {
     return Math.random() * (max - min) + min;
 }
 
-function smoothstep(min: number, max: number, x: number): number {
+export function smoothstep(min: number, max: number, x: number): number {
     if (x <= min) return 0;
     if (x >= max) return 1;
 
@@ -97,7 +97,7 @@ function smoothstep(min: number, max: number, x: number): number {
     return x * x * (3 - 2 * x);
 }
 
-function smootherstep(min: number, max: number, x: number): number {
+export function smootherstep(min: number, max: number, x: number): number {
     if (x <= min) return 0;
     if (x >= max) return 1;
 
