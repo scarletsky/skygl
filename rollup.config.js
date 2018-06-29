66d8abae27406d06db1a0e8d826e37655d1ed9d9
scarletsky <scarletsky1025@gmail.com>
2018-06-29 14:42:14 +0800

    chore: add examples and serve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
 import typescript from 'rollup-plugin-typescript';
 import json from 'rollup-plugin-json';
+import serve from 'rollup-plugin-serve';
 
 export default {
     input: 'src/index.ts',
     output: [
-        { file: 'dist/engine.js', format: 'cjs' }
+        { file: 'dist/engine.js', format: 'umd', name: 'engine' }
     ],
     plugins: [
         typescript({
             typescript: require('typescript')
         }),
-        json()
+        json(),
+        serve({
+            contentBase: './',
+            port: 4444
+        })
     ]
 }
