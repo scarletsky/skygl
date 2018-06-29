aa2959b3dfa1e4f350e5c64e82ed7fb403008632
scarletsky <scarletsky1025@gmail.com>
2018-06-29 11:35:10 +0800

    chore: add index for modules

---
+++ src/math/index.ts
@@ -0,0 +1,5 @@
+export { default as math } from "./math";
+export { default as Vec2 } from "./vec2";
+export { default as Vec3 } from "./vec3";
+export { default as Vec4 } from "./vec4";
+export { default as Mat4 } from "./mat4";
