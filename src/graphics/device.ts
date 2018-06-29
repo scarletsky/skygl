55c1a7f126954c8e6239e8fffd579a8190baf459
scarletsky <scarletsky1025@gmail.com>
2018-06-29 14:49:22 +0800

    feat: auto get webgl context when creating device

--- src/graphics/device.ts
+++ src/graphics/device.ts
@@ -1,12 +1,49 @@
 interface DeviceOptions extends WebGLContextAttributes {
-    webgl2: true
+    preferWebgl2: true
 }
 
 export default class Device {
     public canvas: HTMLCanvasElement;
     public gl: WebGLRenderingContext;
+    public webgl2: boolean;
 
     constructor(canvas: HTMLCanvasElement, options: DeviceOptions = {}) {
         this.canvas = canvas;
+        this.initializeContext(options);
+        this.initializeExtensions();
+        this.initializeCapabilities();
+        this.initializeRenderState();
+    }
+
+    private initializeContext(options: DeviceOptions) {
+        let gl = null;
+        const preferWebgl2 = options.preferWebgl2 !== undefined ? options.preferWebgl2 : true;
+        const names = preferWebgl2 ? ["webgl2", "webgl"] : ["webgl"];
+
+        for (let name of names) {
+            try {
+                gl = this.canvas.getContext(name, options) as WebGLRenderingContext;
+            } catch(err) {}
+
+            if (gl)  {
+                this.gl = gl;
+                this.webgl2 = name === "webgl2";
+                break;
+            }
+        }
+
+        if (!gl) throw new Error("Browser do not support WebGL.");
+    }
+
+    private initializeExtensions() {
+
+    }
+
+    private initializeCapabilities() {
+
+    }
+
+    private initializeRenderState() {
+
     }
 }
