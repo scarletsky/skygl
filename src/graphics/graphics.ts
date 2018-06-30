c75fe09a62997b46dd0f64197977185fb0e7136d
scarletsky <scarletsky1025@gmail.com>
2018-06-30 22:16:12 +0800

    feat: add graphics const

---
+++ src/graphics/graphics.ts
@@ -0,0 +1,40 @@
+export const FUNC_NEVER = 0;
+export const FUNC_LESS = 1;
+export const FUNC_EQUAL = 2;
+export const FUNC_LESSEQUAL = 3;
+export const FUNC_GREATER = 4;
+export const FUNC_NOTEQUAL = 5;
+export const FUNC_GREATEREQUAL = 6;
+export const FUNC_ALWAYS = 7;
+
+export const CLEARFLAG_COLOR = 1;
+export const CLEARFLAG_DEPTH = 2;
+export const CLEARFLAT_STENCIL = 4;
+
+export const BLENDMODE_ZERO = 0;
+export const BLENDMODE_ONE = 1;
+export const BLENDMODE_SRC_COLOR = 2;
+export const BLENDMODE_ONE_MINUS_SRC_COLOR = 3;
+export const BLENDMODE_DST_COLOR = 4;
+export const BLENDMODE_ONE_MINUS_DST_COLOR = 5;
+export const BLENDMODE_SRC_ALPHA = 6;
+export const BLENDMODE_SRC_ALPHA_SATURATE = 7;
+export const BLENDMODE_ONE_MINUS_SRC_ALPHA = 8;
+export const BLENDMODE_DST_ALPHA = 9;
+export const BLENDMODE_ONE_MINUS_DST_ALPHA = 10;
+
+export const BLENDEQUATION_ADD = 0;
+export const BLENDEQUATION_SUBTRACT = 1;
+export const BLENDEQUATION_REVERSE_SUBTRACT = 2;
+export const BLENDEQUATION_MIN = 3;
+export const BLENDEQUATION_MAX = 4;
+
+export const ADDRESS_REPEAT = 0;
+export const ADDRESS_CLAMP_TO_EDGE = 1;
+export const ADDRESS_MIRRORED_REPEAT = 2;
+export const FILTER_NEAREST = 0;
+export const FILTER_LINEAR = 1;
+export const FILTER_NEAREST_MIPMAP_NEAREST = 2;
+export const FILTER_NEAREST_MIPMAP_LINEAR = 3;
+export const FILTER_LINEAR_MIPMAP_NEAREST = 4;
+export const FILTER_LINEAR_MIPMAP_LINEAR = 5;
