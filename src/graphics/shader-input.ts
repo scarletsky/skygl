b85cb4f369d476aa359e401be60f2d14cf1f3ca7
scarletsky <scarletsky1025@gmail.com>
2018-06-30 22:17:37 +0800

    chore: add missing value in shader input

--- src/graphics/shader-input.ts
+++ src/graphics/shader-input.ts
@@ -5,10 +5,12 @@ export default class ShaderInput {
     public locationId: number | WebGLUniformLocation;
     public scopeId: ScopeId;
     public dataType: number;
+    public value: any;
 
     constructor(device: Device, name: string, dataType: number, locationId: number | WebGLUniformLocation) {
         this.locationId = locationId;
         this.scopeId = device.scope.resolve(name);
         this.dataType = dataType;
+        this.value = null;
     }
 }
