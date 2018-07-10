a467adf4232d1a1db000d688589e38916a060422
scarletsky <scarletsky1025@gmail.com>
2018-07-10 19:32:59 +0800

    feat: add EventEmitter

---
+++ src/core/event-emitter.ts
@@ -0,0 +1,69 @@
+export type Callback = (...args: any[]) => void;
+
+interface EventCallback {
+    scope: any;
+    callback: Callback;
+    once: boolean;
+}
+
+interface EventCallbackMap {
+    [event: string]: EventCallback[];
+}
+
+export default class EventEmitter {
+    private _callbacks: EventCallbackMap = {};
+
+    public on(event: string, callback: Callback, once: boolean = false) {
+        if (this._callbacks[event] === undefined) {
+            this._callbacks[event] = [];
+        }
+
+        this._callbacks[event].push({
+            scope: this,
+            callback,
+            once
+        });
+    }
+
+    public once(event: string, callback: Callback) {
+        this.on(event, callback, true);
+    }
+
+    public off(event: string, callback?: Callback) {
+        if (this._callbacks[event] === undefined) return;
+
+        let index = -1;
+        for (let i = 0, length = this._callbacks[event].length; i < length; i++) {
+            if (this._callbacks[event][i].callback === callback) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index === -1) {
+            this._callbacks[event].length = 0;
+        } else {
+            this._callbacks[event].splice(index, 1);
+        }
+    }
+
+    public fire(event: string, ...args: any[]) {
+        const callbacks = this._callbacks[event];
+        const onceFuncIndices: number[] = [];
+
+        if (callbacks === undefined) return;
+
+        callbacks.forEach((callback, i) => {
+            const func = callback.callback;
+            func.apply(this, args);
+
+            if (callback.once) {
+                onceFuncIndices.push(i);
+            }
+        });
+
+        onceFuncIndices.forEach((index, i) => {
+            callbacks.splice(index - i, 1);
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./math";
+export * from "./core";
 export * from "./graphics";
 export * from "./scene";
