ca41d0a65cb7cff72f2eaa89acfd8526eea0a441
scarletsky <scarletsky1025@gmail.com>
2018-11-07 22:14:32 +0800

    fix: add private _direction field in DirectionalLight

--- src/scene/directional-light.ts
+++ src/scene/directional-light.ts
@@ -2,17 +2,18 @@ import Light from "./light";
 import { Vec3, Quat } from "math"
 import { Device } from "graphics";
 
-const vecA = new Vec3();
 const quatA = new Quat();
 
 export default class DirectionalLight extends Light {
     public type = Light.TYPE_DIRECTIONAL;
 
+    private _direction = new Vec3();
+
     public apply(device: Device, index: number) {
         const scope = device.scope;
         this.getWorldRotation(quatA);
-        quatA.transformVector(Vec3.DOWN, vecA);
-        scope.setValue(`uDirectionalLights[0].direction`, vecA);
+        quatA.transformVector(Vec3.DOWN, this._direction);
+        scope.setValue(`uDirectionalLights[0].direction`, this._direction);
         scope.setValue(`uDirectionalLights[${index}].color`, this.color);
     }
 }
