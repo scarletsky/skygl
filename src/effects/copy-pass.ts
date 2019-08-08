2fe739c027305865bba440620678e260bb453441
scarletsky <scarletsky1025@gmail.com>
2019-08-08 18:40:23 +0800

    feat: add RenderPass

---
+++ src/effects/copy-pass.ts
@@ -0,0 +1,43 @@
+import Pass from "./pass";
+import { RenderTarget, Texture } from "graphics";
+import { Renderer } from "renderers";
+
+export default class CopyPass extends Pass {
+    constructor() {
+        super();
+        this.output = new RenderTarget();
+        this.output.attach(RenderTarget.TARGET_COLOR_BUFFER, new Texture());
+    }
+
+    public render(renderer: Renderer, input: RenderTarget) {
+        const device = renderer.device;
+        const gl = device.gl;
+
+        if (!this.output._glFrameBufferId) {
+            this.output.apply(device);
+        }
+
+        if (device.webgl2 && gl instanceof WebGL2RenderingContext) {
+            gl.bindFramebuffer(gl.READ_FRAMEBUFFER, input._glFrameBufferId);
+            gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.output._glFrameBufferId);
+            gl.blitFramebuffer(
+                0, 0, input.colorBuffer.width, input.colorBuffer.height,
+                0, 0, this.output.colorBuffer.width, this.output.colorBuffer.height,
+                gl.COLOR_BUFFER_BIT,
+                gl.NEAREST
+            );
+            gl.bindFramebuffer(gl.READ_FRAMEBUFFER, null);
+            gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+        } else {
+            device.setRenderTarget(input);
+            this.output.colorBuffer.apply(device);
+            gl.copyTexImage2D(
+                gl.TEXTURE_2D, 0, gl.RGBA,
+                0, 0, this.output.colorBuffer.width, this.output.colorBuffer.height,
+                0
+            );
+            device.setRenderTarget(null);
+        }
+
+    }
+}
--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,3 +1,4 @@
 export { default as EffectManager } from "./effect-manager";
 export { default as Pass } from "./pass";
 export { default as RenderPass } from "./render-pass";
+export { default as CopyPass } from "./copy-pass";
