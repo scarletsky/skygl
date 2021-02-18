c8d1d8218e63e8e06f74eacf98301ac61cbc6d81
scarletsky <scarletsky1025@gmail.com>
2021-02-18 17:26:28 +0800

    chore: add npm start command and add serve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
+import serve from 'rollup-plugin-serve';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs'
-  },
-  plugins: [typescript()]
-};
+function generateConfig(name) {
+    const config = {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/skygl.js',
+            format: 'iife',
+            name: 'skygl'
+        },
+        plugins: [
+            typescript(),
+        ]
+    };
+
+    if (name === 'dev') {
+        config.plugins.push(
+            serve({
+                contentBase: './',
+                port: 4444
+            }),
+        );
+    }
+
+    return config;
+}
+
+export default generateConfig(process.env.TARGET);
