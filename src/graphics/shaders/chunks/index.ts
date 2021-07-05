3c2e03f9d2439979e4c8df7a46685ae1307b1b0e
scarletsky <scarletsky1025@gmail.com>
2021-07-05 14:12:42 +0800

    chore: expose some shader chunks

--- src/graphics/shaders/chunks/index.ts
+++ src/graphics/shaders/chunks/index.ts
@@ -4,5 +4,7 @@
 //   When we call `import * as chunks from '...'`,
 //   we will get { common: { default: 'code' }, packingNormal: { default: 'code' }, ... }
 export { default as common } from './common.glsl';
+export { default as vertexDeclare } from './vertexDeclare.glsl';
+export { default as fragmengDeclare } from './fragmentDeclare.glsl';
 export { default as packingNormal } from './packingNormal.glsl';
 export { default as packingDepth } from './packingDepth.glsl';
--- src/graphics/shaders/libs/lambert.vert
+++ src/graphics/shaders/libs/lambert.vert
@@ -1,5 +1,5 @@
-#include <vertexDefinition>
+#include <vertexDeclare>
 
 void main() {
-    gl_Position = sky_ProjectionMatrix * sky_ViewMatrix * sky_ModelMatrix * POSITION;
+    gl_Position = sky_ProjectionMatrix * sky_ViewMatrix * sky_ModelMatrix * vec4(POSITION, 1.0);
 }
