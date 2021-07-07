c7109871c7294c4dfb0079d5edef297262a926ca
scarletsky <scarletsky1025@gmail.com>
2021-07-08 00:12:13 +0800

    feat: improve Color

--- src/math/Color.ts
+++ src/math/Color.ts
@@ -1,31 +1,80 @@
-export interface ColorOptions {
-    r: number;
-    g: number;
-    b: number;
-}
+import { Array3, Array4 } from 'types';
+import { isArray, isNumber } from 'utils';
+import { EPSILON } from './math';
+
+export type RGBOptions = Array3<number>;
+export type RGBAOptions = Array4<number>;
+export type ColorOptions = RGBAOptions;
 
 export class Color {
-    constructor(
-        public r = 0,
-        public g = 0,
-        public b = 0
-    ) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
+    public r = 0;
+    public g = 0;
+    public b = 0;
+    public a = 1;
+
+    static create(options?: RGBOptions | RGBAOptions) {
+        return new Color(options);
     }
 
-    fromJSON(options: ColorOptions) {
-        this.r = options.r;
-        this.g = options.g;
-        this.b = options.b;
+    static isColor(value: any): value is Color {
+        return value instanceof Color;
+    }
+
+    constructor(options?: RGBOptions | RGBAOptions) {
+        this.fromJSON(options);
+    }
+
+    fromJSON(options?: RGBOptions | RGBAOptions) {
+         if (isArray(options)) {
+            if (options.length === 4) this.fromRGBA(options);
+            if (options.length === 3) this.fromRGB(options);
+        }
     }
 
     toJSON() {
-        return {
-            r: this.r,
-            g: this.g,
-            b: this.b
-        };
+        return this.toRGBA();
+    }
+
+    fromRGB(options: RGBOptions) {
+        this.r = isNumber(options[0]) ? options[0] : 0;
+        this.g = isNumber(options[1]) ? options[1] : 0;
+        this.b = isNumber(options[2]) ? options[2] : 0;
+    }
+
+    toRGB(): RGBOptions {
+        return [this.r, this.g, this.b];
+    }
+
+    fromRGBA(options: RGBAOptions) {
+        this.r = isNumber(options[0]) ? options[0] : 0;
+        this.g = isNumber(options[1]) ? options[1] : 0;
+        this.b = isNumber(options[2]) ? options[2] : 0;
+        this.a = isNumber(options[3]) ? options[3] : 1;
+    }
+
+    toRGBA(): RGBAOptions {
+        return [this.r, this.g, this.b, this.a];
+    }
+
+    equals(b: Color) {
+        let a0 = this.r;
+        let a1 = this.g;
+        let a2 = this.b;
+        let a3 = this.a;
+        let b0 = b.r;
+        let b1 = b.g;
+        let b2 = b.b;
+        let b3 = b.a;
+
+        return (
+            Math.abs(a0 - b0) <=
+                EPSILON * Math.max(1.0, Math.abs(a0), Math.abs(b0)) &&
+            Math.abs(a1 - b1) <=
+                EPSILON * Math.max(1.0, Math.abs(a1), Math.abs(b1)) &&
+            Math.abs(a2 - b2) <=
+                EPSILON * Math.max(1.0, Math.abs(a2), Math.abs(b2)) &&
+            Math.abs(a3 - b3) <=
+                EPSILON * Math.max(1.0, Math.abs(a3), Math.abs(b3))
+        );
     }
 }
