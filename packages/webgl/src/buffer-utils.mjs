490aabb8e5d50994b95828fe9c4597bbb95c43e3
scarletsky <scarletsky1025@gmail.com>
2024-04-26 17:10:30 +0800

    feat: add buffer and buffer-utils

---
+++ packages/webgl/src/buffer-utils.mjs
@@ -0,0 +1,17 @@
+import { STATIC_DRAW } from './constants.mjs';
+
+export function createGLBuffer(gl) {
+  return gl.createBuffer();
+}
+
+export function setGLBufferData(gl, glBuffer, target, dataOrSize, usage = STATIC_DRAW, shouldBindBuffer = true) {
+  if (shouldBindBuffer) gl.bindBuffer(target, glBuffer);
+  gl.bufferData(target, dataOrSize, usage);
+  return glBuffer;
+}
+
+export function setGLBufferSubData(gl, glBuffer, target, offset, data, shouldBindBuffer = true) {
+  if (shouldBindBuffer) gl.bindBuffer(target, glBuffer);
+  gl.bufferSubData(target, offset, data)
+  return glBuffer;
+}
---
+++ packages/webgl/src/buffer.mjs
@@ -0,0 +1,72 @@
+import { STATIC_DRAW } from './constants.mjs';
+import { bindBuffer } from './context-utils.mjs';
+import {
+  createGLBuffer,
+  setGLBufferData,
+  setGLBufferSubData
+} from './buffer-utils.mjs';
+
+export class Buffer {
+  constructor(options = {}) {
+    this.gl = options.gl || null;
+    this.target = options.target;
+    this.usage = options.usage || STATIC_DRAW;
+    this.data = null;
+    this.byteLength = options.byteLength || 0;
+    this.interleaved = null;
+    this.glBuffer = null;
+
+    setBufferData(this.gl, this, options.data);
+  }
+
+  setData(data) {
+    return setBufferData(this.gl, this, data);
+  }
+}
+
+Buffer.prototype.isBuffer = true;
+
+export function createBuffer(gl, target, data, options = {}) {
+  return new Buffer({ gl, target, data, ...options });
+}
+
+export function deleteBuffer(gl, buffer) {
+  if (!buffer.glBuffer) return false;
+
+  gl.deleteBuffer(buffer.glBuffer);
+  buffer.glBuffer = null;
+
+  return true;
+}
+
+export function setBufferData(gl, buffer, data) {
+  if (buffer.data !== data) buffer.data = data;
+
+  if (!buffer.glBuffer) {
+    buffer.glBuffer = createGLBuffer(gl);
+  }
+
+  bindBuffer(gl, buffer);
+  setGLBufferData(gl, buffer.glBuffer, buffer.target, (buffer.data || buffer.size), buffer.usage, false);
+
+  return true;
+}
+
+export function setBufferSubData(_gl, buffer, offset, data) {
+  if (!buffer.glBuffer) {
+    buffer.glBuffer = createGLBuffer(gl);
+  }
+
+  bindBuffer(gl, buffer);
+  setGLBufferSubData(gl, buffer.glBuffer, buffer.target, offset, data, false);
+
+  return true;
+}
+
+export function toInterleavedBuffer(buffers) {
+
+}
+
+export function toSeparateBuffers(interleavedBuffer) {
+
+}
