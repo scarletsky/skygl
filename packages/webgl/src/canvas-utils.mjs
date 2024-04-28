486e821aef608df8c0eae5636d633ca2e1319c93
scarletsky <scarletsky1025@gmail.com>
2024-04-28 14:43:43 +0800

    feat: add setCanvasResolution API

--- packages/webgl/src/canvas-utils.mjs
+++ packages/webgl/src/canvas-utils.mjs
@@ -1,3 +1,10 @@
-export function resizeCanvas(canvas, width, height) {
-  
+export function setCanvasResolution(canvas, resolution = 1) {
+  const width  = canvas.clientWidth  * resolution | 0;
+  const height = canvas.clientHeight * resolution | 0;
+
+  if (canvas.width === width && canvas.height === height) return false;
+
+  canvas.width = width;
+  canvas.height = height;
+  return true;
 }
